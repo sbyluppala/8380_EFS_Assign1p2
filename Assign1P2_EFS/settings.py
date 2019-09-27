d5774baae068a3e60f2763fe0639ec4d36700fb0
swetha <sbyluppala@unomaha.edu>
2019-09-27 10:51:14 -0500

    updated settings file

--- Assign1P2_EFS/settings.py
+++ Assign1P2_EFS/settings.py
@@ -92,12 +92,7 @@ WSGI_APPLICATION = 'Assign1P2_EFS.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+
 
 
 # Password validation
